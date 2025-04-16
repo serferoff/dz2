11458c3948d7041d50fb714a54f681e49df61593
serfer <serferofficial@gmail.com>
2025-04-16 20:37:51 +0300

    "добавлены комментарии"

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,8 @@
-# функция маскировки номера карты
 def get_mask_card_number(card_number: str) -> str:
+    """функция маскировки номера карты"""
 
     return card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
 
-
-# функция маскировки номера аккаунта
 def get_mask_account(account_number: str) -> str:
-
+    """функция маскировки номера аккаунта"""
     return "**" + account_number[-4:]
